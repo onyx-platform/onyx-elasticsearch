7e511fc453c344fe7e0a445a4449e467279c920d
Matt Anderson <manderson@liaison.com>
2015-11-04 10:16:17 -0500

    updated project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,16 @@
-(defproject org.onyxplatform/onyx-elasticsearch "0.7.14.0"
+(defproject com.liaison/onyx-elasticsearch "0.7.14.0"
   :description "Onyx plugin for Elasticsearch"
-  :url "https://github.com/manderson202/onyx-elasticsearch"
+  :url "https://github.com/LiaisonTechnologies/onyx-elasticsearch"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"snapshots" {:url "https://clojars.org/repo"
+                              :username :env/clojars_username
+                              :password :env/clojars_password
+                              :sign-releases false}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env/clojars_username
+                             :password :env/clojars_password
+                             :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.onyxplatform/onyx "0.7.14"]
                  [clojurewerkz/elastisch "2.2.0-beta4"]
