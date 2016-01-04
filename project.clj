6503928134c78e1f18bf1e7072e741f7ef1d3450
Matt Anderson <manderson@liaison.com>
2016-01-04 09:35:15 -0500

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-elasticsearch "0.8.3.2"
+(defproject org.onyxplatform/onyx-elasticsearch "0.8.3.3-SNAPSHOT"
   :description "Onyx plugin for Elasticsearch"
   :url "https://github.com/onyx-platform/onyx-elasticsearch"
   :license {:name "Eclipse Public License"
