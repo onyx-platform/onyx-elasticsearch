209cb59fa17e94b287c76f31224fb8d0767a3e69
OnyxBot <michael.drogalis@onyxplatform.org>
2016-12-01 17:37:10 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-elasticsearch "0.9.15.0"
+(defproject org.onyxplatform/onyx-elasticsearch "0.9.15.1-SNAPSHOT"
   :description "Onyx plugin for Elasticsearch"
   :url "https://github.com/onyx-platform/onyx-elasticsearch"
   :license {:name "Eclipse Public License"
